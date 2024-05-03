81e577134161d65c73550d5d4cd054238535c247
pedrogbraz <pedrobrazdevs@gmail.com>
2024-05-03 01:31:38 -0300

    feat: add prisma file

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,11 @@
+const CategoryList = () => {
+  return ( 
+    // Pegar categorias do banco de dados
+
+    // Renderizar um item para cada categoria
+   <>
+   </>
+   );
+}
+ 
+export default CategoryList;

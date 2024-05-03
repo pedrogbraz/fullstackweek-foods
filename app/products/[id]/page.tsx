e570d5d98e243d8d07f32f942570e6577b0acb50
pedrogbraz <pedrobrazdevs@gmail.com>
2024-05-03 16:35:42 -0300

    feat: ensure juice from product restaurants are listed

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -19,20 +19,23 @@ const ProductsPage = async ({ params: { id } }: ProductsPageProps) => {
     },
   });
 
+  if (!product) {
+    return notFound();
+  }
+
   const juices = await db.product.findMany({
     where: {
       category: {
         name: "Sucos",
+      },
+      restaurant: {
+        id: product?.restaurant.id
       }
     },
     include: {
       restaurant: true,
     },
   });
-
-  if (!product) {
-    return notFound();
-  }
 
   return (
     <div>
